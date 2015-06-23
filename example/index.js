f57acc722e70048d3b8fba5c906786fb2a07e206
Ionică Bizău <bizauionica@yahoo.com>
2015-06-23 12:02:22 +0300

    Use bug-killer in example

--- example/index.js
+++ example/index.js
@@ -1,7 +1,10 @@
 // Dependencies
-var FileWatcher = require("../lib");
+var FileWatcher = require("../lib")
+  , Logger = require("bug-killer")
+  ;
 
 // Listen for changes
-FileWatcher(__dirname + "/test.txt", function (err, ev) {
-    console.log(err || ev);
+FileWatcher(__dirname + "/test.txt", function (err, ev, path) {
+    if (err) { return Logger.log(err, "error"); }
+    Logger.log([ev, path].join(" "));
 });
