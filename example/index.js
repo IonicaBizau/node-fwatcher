fd204d418ae041860d3f5a231a5ab3f8ef9ddc0b
Ionică Bizău <bizauionica@yahoo.com>
2015-06-23 12:10:20 +0300

    Updated example

--- example/index.js
+++ example/index.js
@@ -3,8 +3,16 @@ var FileWatcher = require("../lib")
   , Logger = require("bug-killer")
   ;
 
-// Listen for changes
-FileWatcher(__dirname + "/test.txt", function (err, ev, path) {
+// Listen for changes for 5 seconds
+var watcher = FileWatcher(__dirname + "/test.txt", function (err, ev, path) {
+    if (err) { return Logger.log(err, "error"); }
+    Logger.log([ev, path].join(" "));
+});
+setTimeout(watcher.off.bind(watcher), 5000);
+
+
+// Listen only for one change
+FileWatcher(__dirname + "/once.txt", true, function (err, ev, path) {
     if (err) { return Logger.log(err, "error"); }
     Logger.log([ev, path].join(" "));
 });
